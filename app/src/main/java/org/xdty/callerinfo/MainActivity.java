ae00edb6164ea6a57d8ef962e3292c9319be80c0
tianyu <xdtianyu@gmail.com>
2015-12-27 19:38:49 +0800

    fix android M permission

--- app/src/main/java/org/xdty/callerinfo/MainActivity.java
+++ app/src/main/java/org/xdty/callerinfo/MainActivity.java
@@ -1,7 +1,16 @@
 package org.xdty.callerinfo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.xdty.phone.number.PhoneNumber;
 import org.xdty.phone.number.model.NumberInfo;
@@ -9,6 +18,10 @@ import org.xdty.phone.number.model.NumberInfo;
 import wei.mark.standout.StandOutWindow;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String TAG = MainActivity.class.getSimpleName();
+    public final static int REQUEST_CODE_OVERLAY_PERMISSION = 1001;
+    public final static int REQUEST_CODE_ASK_PERMISSIONS = 1002;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,56 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE_OVERLAY_PERMISSION);
+            }
+
+            int res = checkSelfPermission(Manifest.permission.READ_PHONE_STATE);
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
+                        REQUEST_CODE_OVERLAY_PERMISSION);
+            }
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        StandOutWindow.closeAll(this, FloatWindow.class);
+        super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_OVERLAY_PERMISSION) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Log.e(TAG, "SYSTEM_ALERT_WINDOW permission not granted...");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+            @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "READ_PHONE_STATE Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void showFloatWindow() {
         StandOutWindow.closeAll(this, FloatWindow.class);
         new PhoneNumber(this, new PhoneNumber.Callback() {
             @Override
@@ -37,11 +100,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }).fetch("10086");
-    }
-
-    @Override
-    protected void onStop() {
-        StandOutWindow.closeAll(this, FloatWindow.class);
-        super.onStop();
     }
 }
