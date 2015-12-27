59750b93d3641db8dacd02d511604c345681a9f3
tianyu <xdtianyu@gmail.com>
2015-12-27 21:18:42 +0800

    add search

--- app/src/main/java/org/xdty/callerinfo/MainActivity.java
+++ app/src/main/java/org/xdty/callerinfo/MainActivity.java
@@ -1,15 +1,27 @@
 package org.xdty.callerinfo;
 
 import android.Manifest;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import org.xdty.phone.number.PhoneNumber;
@@ -23,10 +35,23 @@ public class MainActivity extends AppCompatActivity {
     public final static int REQUEST_CODE_OVERLAY_PERMISSION = 1001;
     public final static int REQUEST_CODE_ASK_PERMISSIONS = 1002;
 
+    Toolbar toolbar;
+
+    private int mScreenWidth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        WindowManager mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = mWindowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+
+        mScreenWidth = point.x;
     }
 
     @Override
@@ -81,7 +106,51 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void showFloatWindow() {
+    private SearchView mSearchView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mSearchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.setMaxWidth(mScreenWidth);
+        mSearchView.setInputType(InputType.TYPE_CLASS_NUMBER);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d(TAG, "onQueryTextSubmit: " + query);
+                showFloatWindow(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                StandOutWindow.closeAll(MainActivity.this, FloatWindow.class);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                break;
+            case R.id.action_float_window:
+                showFloatWindow("10086");
+                Snackbar.make(toolbar, R.string.float_window_hint, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                break;
+        }
+
+        return true;
+    }
+
+    private void showFloatWindow(String phoneNumber) {
         StandOutWindow.closeAll(this, FloatWindow.class);
         new PhoneNumber(this, new PhoneNumber.Callback() {
             @Override
@@ -99,6 +168,6 @@ public class MainActivity extends AppCompatActivity {
             public void onResponseFailed(NumberInfo numberInfo) {
 
             }
-        }).fetch("10086");
+        }).fetch(phoneNumber);
     }
 }
