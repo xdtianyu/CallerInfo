16432f9501b4004743680ec0da4c1fda68555ea3
tianyu <xdtianyu@gmail.com>
2016-04-11 21:27:41 +0800

    update application

--- app/src/debug/java/org/xdty/callerinfo/application/DebugApplication.java
+++ app/src/debug/java/org/xdty/callerinfo/application/DebugApplication.java
@@ -1,16 +1,10 @@
 package org.xdty.callerinfo.application;
 
-import android.content.SharedPreferences;
 import android.os.StrictMode;
-import android.preference.PreferenceManager;
 
 import com.facebook.stetho.Stetho;
 
-import org.xdty.callerinfo.R;
-
-import cat.ereza.customactivityoncrash.CustomActivityOnCrash;
-
-public class DebugApplication extends com.orm.SugarApp {
+public class DebugApplication extends Application {
     public final static String TAG = Application.class.getSimpleName();
 
     @Override
@@ -27,16 +21,11 @@ public class DebugApplication extends com.orm.SugarApp {
                 .penaltyDeath()
                 .build());
 
-        super.onCreate();
         Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
                         .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
                         .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
                         .build());
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean isCatch = pref.getBoolean(getString(R.string.catch_crash_key), false);
-        if (isCatch) {
-            CustomActivityOnCrash.install(this);
-        }
+        super.onCreate();
     }
 }
