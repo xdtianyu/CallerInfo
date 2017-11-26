e7d0355993ef7ac7afd0af2724897aa18a90f005
tianyu <xdtianyu@gmail.com>
2017-11-26 15:51:36 +0800

    fix float window on android oreo

--- app/src/main/java/org/xdty/callerinfo/model/permission/PermissionImpl.java
+++ app/src/main/java/org/xdty/callerinfo/model/permission/PermissionImpl.java
@@ -5,10 +5,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.WindowManager;
 
 public class PermissionImpl implements Permission {
 
@@ -20,10 +23,37 @@ public class PermissionImpl implements Permission {
 
     @Override
     public boolean canDrawOverlays() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        } else if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             return Settings.canDrawOverlays(mContext);
+        } else {
+            if (Settings.canDrawOverlays(mContext)) {
+                return true;
+            }
+            try {
+                WindowManager mgr = (WindowManager) mContext.getSystemService(
+                        Context.WINDOW_SERVICE);
+                if (mgr == null) {
+                    return false; //getSystemService might return null
+                }
+                View viewToAdd = new View(mContext);
+                WindowManager.LayoutParams params = new WindowManager.LayoutParams(0, 0,
+                        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ?
+                                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+                                : WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                        PixelFormat.TRANSPARENT);
+                viewToAdd.setLayoutParams(params);
+                mgr.addView(viewToAdd, params);
+                mgr.removeView(viewToAdd);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
         }
-        return true;
     }
 
     @Override
