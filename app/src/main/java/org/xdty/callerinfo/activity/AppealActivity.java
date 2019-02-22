2ae80873f4096c2441fa0f8a15d959593fd171fb
tianyu <xdtianyu@gmail.com>
2019-02-22 22:20:46 +0800

    add number appeal
    Signed-off-by: tianyu <xdtianyu@gmail.com>

---
+++ app/src/main/java/org/xdty/callerinfo/activity/AppealActivity.java
@@ -0,0 +1,15 @@
+package org.xdty.callerinfo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.xdty.callerinfo.R;
+
+public class AppealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appeal);
+    }
+}
