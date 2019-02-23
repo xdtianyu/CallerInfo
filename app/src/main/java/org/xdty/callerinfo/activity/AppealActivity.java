19eb93c30ad362f3e8b847b0549b098f986eed2f
tianyu <xdtianyu@gmail.com>
2019-02-23 12:18:21 +0800

    add appeal activity

--- app/src/main/java/org/xdty/callerinfo/activity/AppealActivity.java
+++ app/src/main/java/org/xdty/callerinfo/activity/AppealActivity.java
@@ -1,15 +1,47 @@
 package org.xdty.callerinfo.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
 
 import org.xdty.callerinfo.R;
 
-public class AppealActivity extends AppCompatActivity {
+public class AppealActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton mAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appeal);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mAddButton = findViewById(R.id.add);
+
+        mAddButton.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add:
+                break;
+        }
     }
 }
