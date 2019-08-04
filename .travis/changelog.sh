c53b4e850b479f3477caf1e9cbcf3edb0d054675
tianyu <xdtianyu@gmail.com>
2019-08-04 23:56:35 +0800

    add changes link

--- .travis/changelog.sh
+++ .travis/changelog.sh
@@ -11,21 +11,26 @@ if [ ! -z "$TRAVIS_BUILD_ID" ]; then
 fi
 
 echo "
-[![Download (github release)](https://img.shields.io/github/downloads/xdtianyu/CallerInfo/$TAG/total.svg)](https://github.com/xdtianyu/CallerInfo/releases/download/$TAG/callerinfo-$TAG-release.apk)
-[![Download (github release)](https://img.shields.io/github/downloads/xdtianyu/CallerInfo/$TAG/CallerInfo-plugin-v$PLUGIN_VERSION-full-release.apk.svg)](https://github.com/xdtianyu/CallerInfo/releases/download/$TAG/CallerInfo-plugin-v$PLUGIN_VERSION-full-release.apk)
+[![Download (github release)](https://img.shields.io/github/downloads/$TRAVIS_REPO_SLUG/$TAG/total.svg)](https://github.com/$TRAVIS_REPO_SLUG/releases/download/$TAG/callerinfo-$TAG-release.apk)
+[![Download (github release)](https://img.shields.io/github/downloads/$TRAVIS_REPO_SLUG/$TAG/CallerInfo-plugin-v$PLUGIN_VERSION-full-release.apk.svg)](https://github.com/$TRAVIS_REPO_SLUG/releases/download/$TAG/CallerInfo-plugin-v$PLUGIN_VERSION-full-release.apk)
 
 ---
 
 Release files are generated and deployed by Travis-ci, check sha1 and md5 from **build log:**
 
-[https://travis-ci.org/xdtianyu/CallerInfo/builds/$TRAVIS_BUILD_ID#L$MD5_LINE](https://travis-ci.org/xdtianyu/CallerInfo/builds/$TRAVIS_BUILD_ID#L$MD5_LINE)
+[https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID#L$MD5_LINE](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID#L$MD5_LINE)
 "
 
 echo "## commits"
 git --no-pager log $PREV_TAG...$TAG \
-    --pretty=format:' - [%s](https://github.com/xdtianyu/CallerInfo/commit/%H)' \
+    --pretty=format:' - [%s](https://github.com/$TRAVIS_REPO_SLUG/commit/%H)' \
     --reverse | \
     grep -v 'Merge pull request' | \
     grep -v 'build' | \
     grep -v 'bump'
 
+echo "
+## changes
+
+- [$PREV_TAG - $TAG](https://github.com/$TRAVIS_REPO_SLUG/compare/$PREV_TAG...$TAG?diff=unified)
+"
